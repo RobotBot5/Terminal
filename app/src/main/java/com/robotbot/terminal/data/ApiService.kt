12d2ceefdb63b0d6737cbc59101ed38f05a06f81
Ilya Kazakov <robotbotyourube@gmail.com>
2025-01-05 02:48:25 +0300

    Add scaling

--- app/src/main/java/com/robotbot/terminal/data/ApiService.kt
+++ app/src/main/java/com/robotbot/terminal/data/ApiService.kt
@@ -4,7 +4,7 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2023-01-09/2024-01-09?adjusted=true&sort=asc&limit=50000&apiKey=6UpSfjXVxIVpmbnwqtjNumCfv51QPHTK")
+    @GET("aggs/ticker/AAPL/range/1/hour/2023-01-09/2024-01-09?adjusted=true&sort=desc&limit=50000&apiKey=6UpSfjXVxIVpmbnwqtjNumCfv51QPHTK")
     suspend fun loadBars(): Result
 
 }
