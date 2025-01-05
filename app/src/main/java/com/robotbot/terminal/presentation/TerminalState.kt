8d264b6e2b999d6ff7c5f9adcdf7cd3380a70866
Ilya Kazakov <robotbotyourube@gmail.com>
2025-01-05 18:34:13 +0300

    Create TerminalState

---
+++ app/src/main/java/com/robotbot/terminal/presentation/TerminalState.kt
@@ -0,0 +1,35 @@
+package com.robotbot.terminal.presentation
+
+import android.os.Parcelable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.robotbot.terminal.data.Bar
+import kotlinx.parcelize.Parcelize
+import kotlin.math.roundToInt
+
+@Parcelize
+data class TerminalState(
+    val barList: List<Bar>,
+    val visibleBarCount: Int = 100,
+    val terminalWidth: Float = 0f,
+    val scrolledBy: Float = 0f
+) : Parcelable {
+    val barWidth: Float
+        get() = terminalWidth / visibleBarCount
+
+    val visibleBars: List<Bar>
+        get() {
+            val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(0)
+            val endIndex = (startIndex + visibleBarCount).coerceAtMost(barList.size)
+            return barList.subList(startIndex, endIndex)
+        }
+}
+
+@Composable
+fun rememberTerminalState(bars: List<Bar>): MutableState<TerminalState> {
+    return rememberSaveable {
+        mutableStateOf(TerminalState(bars))
+    }
+}
